24c4c4691cadb0fe5e5b16e7cce900e40c1642c4
Emmanuel-A25 <esiziba71@gmail.com>
2025-10-07 13:04:34 +0200

    1.cleaned up the HTML file. 2.Fixed the JavaScript file to dynamically populate the user dropdown and display agendas based on the selected user.

--- common.mjs
+++ common.mjs
@@ -1,36 +1,27 @@
- /*export function getUserIds() {
-  return ["1", "2", "3", "4", "5"];
-}*/
- // commented out and focused on the logic of project, will look at the instructions in the rubric in the morning and talk about it during a huddle. 
-//theres something i do not get here. 
 
-const dropdown = document.getElementById("userDropdown");
-
-dropdown.addEventListener("change", function() {
-  const selectedUserId = dropdown.value; 
-  console.log("Selected user ID:", selectedUserId);//adding a event listener to the drop down wich means this function will run everytime a user selects a new option.
-
-//load agenda for this user
-const agenda = getData(selectedUserId);
-console.log("Agenda", agenda);
-
-//handling the no agenda message instruction
-const msg = document.getElementById("noAgendaMsg");
-if (agenda.length === 0) {
-  msg.style.display = "block";
-  return "no agenda";
+ export function getUserIds() {
+  return ["user1", "user2", "user3", "user4", "user5"];
 }
- else {
-  msg.style.display = "none"; // added this because it hides the no agenda messages when theres actual data. 
- }
-
- //arranging the dates in chronological order and filtering out the past dates
-const today = new Date();
-
-
-const future = agenda.filter(item => new Date(item.date) >= today); //filter() creates a new array with the selected data.It ensures that the table doesnt display old versions or old dates. 
-//sort the future agenda chronologically
-future.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   
-});
+  export function getData(userId) {
+  const agendaData = {
+    //users data
+  }
+  
+
+  return agendaData[userId] || [];
+  }
+// in this function we filter and sort out future events.
+  export function getFutureAgendas(agenda){
+    const today = new Date();
+  
+
+  //filter out past dates 
+  const future = agenda.filter(item => new Date(item.date) >= today);
+
+  //sort future dates chronologically
+  future.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  return future;
+}
